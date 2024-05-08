bd5c1b6c1d64371364ea9acdabcacf691b476c3c
dostavic <dmytro.havrysh@outlook.com>
2024-05-08 17:36:24 +0200

    done

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-from fastapi import FastAPI, WebSocket
-from typing import List
-from azure.eventhub import EventHubConsumerClient
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from azure.eventhub.aio import EventHubConsumerClient
+import asyncio
 
 app = FastAPI()
 
-
-# Define your event processing callback
-async def on_event(partition_context, event):
-    return event.body_as_str()
-
-
-@app.websocket("/")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+async def handle_event_hub(websocket):
     connection_str = 'Endpoint=sb://iothub-ns-itestuniqu-58220205-ef5518ff0b.servicebus.windows.net/;SharedAccessKeyName=iothubowner;SharedAccessKey=LwC9nt7L2fMVMVC1NeoOZSAHSLtcICG7CAIoTPZYIbo=;EntityPath=itestunique123452'
-    consumer_group = "$Default"  # The consumer group you want to read from
+    consumer_group = "$Default"
     eventhub_name = "itestunique123452"
-    # Create an instance of EventHubConsumerClient
+
     client = EventHubConsumerClient.from_connection_string(
         conn_str=connection_str,
         consumer_group=consumer_group,
         eventhub_name=eventhub_name
     )
 
-    try:
-        # Start the consumer
-        async with client:
-            # Receive events from all partitions asynchronously
-            async for event in client.receive(on_event=on_event):
-                print(event)
-                await on_event(None, event, websocket)
-    except Exception :
-        client.close()
-        print(Exception + "AAAA")
-        return
+    async def on_event(partition_context, event):
+        if event:
+            event_data = event.body_as_str()
+            print(f"Received event: {event_data}")
+            await websocket.send_text(f"Received event: {event_data}")
+        else:
+            print("No event received.")
 
-    client.close()
+    async with client:
+        await client.receive(on_event=on_event, starting_position="-1")
+
+@app.websocket("/")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        await handle_event_hub(websocket)
+    except WebSocketDisconnect:
+        print("WebSocket disconnected")
+    finally:
+        await websocket.close()
