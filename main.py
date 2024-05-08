a90d439f6c37ded4349e930edb2db776ab8070f7
dostavic <dmytro.havrysh@outlook.com>
2024-05-08 18:22:07 +0200

    DefaultAzureCredential

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from azure.eventhub.aio import EventHubConsumerClient
 import asyncio
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.eventhub import EventHubManagementClient
 
 app = FastAPI()
+
+credential = DefaultAzureCredential()
+subscription_id = '957bbb97-844e-40d6-8155-5dc5b9ddaf69'
+
+client = EventHubManagementClient(credential, subscription_id)
+
+resource_group_name = 'smart-house'
+namespace_name = 'itestunique123452'
+policy_name = 'iothubowner'
+
+key = client.namespaces.list_keys(resource_group_name, namespace_name, policy_name)
+connection_string = key.primary_connection_string
+
+print("Connection String:", connection_string)
 
 async def handle_event_hub(websocket):
     connection_str = 'Endpoint=sb://iothub-ns-itestuniqu-58220205-ef5518ff0b.servicebus.windows.net/;SharedAccessKeyName=iothubowner;SharedAccessKey=LwC9nt7L2fMVMVC1NeoOZSAHSLtcICG7CAIoTPZYIbo=;EntityPath=itestunique123452'
@@ -19,7 +35,7 @@ async def handle_event_hub(websocket):
         if event:
             event_data = event.body_as_str()
             print(f"Received event: {event_data}")
-            await websocket.send_text(f"Received event: {event_data}")
+            await websocket.send_text(str(event))
         else:
             print("No event received.")
 
