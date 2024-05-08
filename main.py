581e91ef7863c863e39a8490affc00ed1d9a04b6
dostavic <dmytro.havrysh@outlook.com>
2024-05-08 19:28:26 +0200

    DefaultAzureCredential + EventHub

--- main.py
+++ main.py
@@ -3,25 +3,45 @@ from azure.eventhub.aio import EventHubConsumerClient
 import asyncio
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.eventhub import EventHubManagementClient
+from azure.mgmt.iothub import IotHubClient
 
 app = FastAPI()
 
 credential = DefaultAzureCredential()
 subscription_id = '957bbb97-844e-40d6-8155-5dc5b9ddaf69'
 
-client = EventHubManagementClient(credential, subscription_id)
+client = IotHubClient(credential, subscription_id)
 
 resource_group_name = 'smart-house'
-namespace_name = 'itestunique123452'
+iot_hub_name = 'itestunique123452'
+
 policy_name = 'iothubowner'
 
-key = client.namespaces.list_keys(resource_group_name, namespace_name, policy_name)
-connection_string = key.primary_connection_string
+keys = client.iot_hub_resource.get_keys_for_key_name(resource_group_name, iot_hub_name, policy_name)
 
-print("Connection String:", connection_string)
+iot_hub = client.iot_hub_resource.get(resource_group_name, iot_hub_name)
+
+properties = iot_hub.properties
+endpoints = properties.event_hub_endpoints
+endpoint = None
+
+if 'events' in endpoints:
+    endpoint = endpoints['events'].endpoint
+    print("Endpoint:", endpoint)
+else:
+    print("Event Hub endpoint not found.")
+
+connection_str = (
+    f"Endpoint={endpoint};"
+    f"SharedAccessKeyName={policy_name};"
+    f"SharedAccessKey={keys.primary_key};"
+    f"EntityPath={iot_hub_name}"
+)
+
+print("Connection String:", connection_str)
 
 async def handle_event_hub(websocket):
-    connection_str = 'Endpoint=sb://iothub-ns-itestuniqu-58220205-ef5518ff0b.servicebus.windows.net/;SharedAccessKeyName=iothubowner;SharedAccessKey=LwC9nt7L2fMVMVC1NeoOZSAHSLtcICG7CAIoTPZYIbo=;EntityPath=itestunique123452'
+    # connection_str = 'Endpoint=sb://iothub-ns-itestuniqu-58220205-ef5518ff0b.servicebus.windows.net/;SharedAccessKeyName=iothubowner;SharedAccessKey=LwC9nt7L2fMVMVC1NeoOZSAHSLtcICG7CAIoTPZYIbo=;EntityPath=itestunique123452'
     consumer_group = "$Default"
     eventhub_name = "itestunique123452"
 
